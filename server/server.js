2f795e8123381bc0910fc38ab421526382ef1662
Tiffani Underwood <tdunderwood217@gmail.com>
2024-01-27 15:18:08 -0500

    deleted node_modules added resolvers.js and typeDefs.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,39 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const { ApolloServer } = require('@applo/server');
-const { expressMiddleware } = require('@applo/server/express4');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+// const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  // typeDefs,
+  // resolvers,
   context: authMiddleware,
 });
+
+const startApolloServer = async () => {
+  await server.start();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/graphql', expressMiddleware(server,{ context: authMiddleware}));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
+  // 'build' is changed to 'dist' in the example
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+};
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+};
+
+startApolloServer();
